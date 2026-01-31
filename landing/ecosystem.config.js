2c7b9a2d22642c42a85db70058fd9ff58420234c
reonaldi152 <reonaldi152@gmail.com>
2026-01-31 16:07:14 +0700

    feat: add ecosystem configuration for bengkel-app and bengkel-landing

---
+++ landing/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "bengkel-landing",
+      script: "serve",
+      args: "dist -s -l 3005",
+      cwd: "./",
+      env: {
+        NODE_ENV: "production",
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M",
+    },
+  ],
+};
